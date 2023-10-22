e58ff4eaa65493f9ae4cb4d70a82ef939da0ac04
fernandoferra <fernandofaugusto@gmail.com>
2023-10-22 15:57:30 -0300

    commit livro

---
+++ src/listas/Tarefa.java
@@ -0,0 +1,21 @@
+package listas;
+
+public class Tarefa {
+    
+    public String descricao;
+
+    public Tarefa(String descricao) {
+        this.descricao = descricao;
+    }
+
+    public String getDescricao() {
+        return descricao;
+    }
+
+    @Override
+    public String toString() {
+        return "Tarefa (" + " descrição " + descricao + ")";
+    }
+}
+
+   
